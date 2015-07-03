3e5100f1ad438ee9fa5871bed3897933f9f7ffc4
Jack Keating <keatjm@gmail.com>
2015-07-02 19:36:57 -0500

    config var for app settings

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import os
 from app import create_app
 from app.extensions import db
 
-app = create_app('config.development')  # TODO: config var
+app = create_app(os.environ['APP_SETTINGS'])
 
 migrate = Migrate(app, db)
 manager = Manager(app)
