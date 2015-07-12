dfb2868a7328b3c77dd572e6c0f347eacf51679f
Jack Keating <keatjm@gmail.com>
2015-07-12 17:29:01 -0500

    check if field.data of amount is none (which is the case with decimals)

--- app/forms/nickels.py
+++ app/forms/nickels.py
@@ -22,12 +22,13 @@ class SendForm(Form):
             raise ValidationError("That user doesn't exist!")
 
     def validate_amount(self, field):
-        if field.data < 1:
-            raise ValidationError("You need to send at least 1 nickel.")
-        elif current_user.main_wallet.nickels == 0:
-            raise ValidationError("You don't have any nickels in your wallet.")
-        elif field.data > current_user.main_wallet.nickels:  # TODO: had 2 nickels, got error "you only have 2 nickels"
-            raise ValidationError("You only have {0} nickels.".format(current_user.main_wallet.nickels))
+        if field.data:
+            if field.data < 1:
+                raise ValidationError("You need to send at least 1 nickel.")
+            elif current_user.main_wallet.nickels == 0:
+                raise ValidationError("You don't have any nickels in your wallet.")
+            elif field.data > current_user.main_wallet.nickels:  # TODO: had 2 nickels, got error "you only have 2 nickels"
+                raise ValidationError("You only have {0} nickels.".format(current_user.main_wallet.nickels))
 
     def transfer_nickels(self):
         current_user.main_wallet.subtract(self.amount.data)
